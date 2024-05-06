041557f4d33ac4ec6e7c40c6f79b5f8a85e4ea30
chiragjain307 <chiragjain.jain7@gmail.com>
2024-05-06 16:13:39 +0530

    first

---
+++ Client/src/App.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import io from 'socket.io-client'
+
+const socket = io.connect('http://192.168.0.106:3001')
+
+function App() {
+  const [message, setMessage] = useState('')
+  const [room, setRoom] = useState('')
+  const [receivedMessage, setReceivedMessage] = useState('')
+   
+  const handleRoom = () => {
+    if(room === '') return
+    else socket.emit('join_room', room)
+  }
+
+  const handleButton = () => {
+    socket.emit('send_message', {message, room})
+    setMessage('')
+    
+  }
+  
+  useEffect(() => {
+    socket.on('receive_message', (data) => {
+      setReceivedMessage(data.message)
+      setRoom(data.room)
+    })
+  }, [socket])
+  
+  return (
+    <>
+        <div>
+          <input type="text" placeholder='Room...' onChange={(e)=>setRoom(e.target.value)} value={room} />
+          <button onClick={handleRoom}>Join Room</button>
+          <h1>Room No.: {room}</h1>
+          
+          <input type="text" placeholder='Message...' onChange={(e)=>setMessage(e.target.value)} value={message}/>
+          <button onClick={handleButton}>Send Message</button>
+          <h1>Message: {receivedMessage}</h1>
+          
+        </div>
+    </>
+  )
+}
+
+export default App
---
+++ Server/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+
+const http = require('http')
+const {Server} = require('socket.io')
+
+const cors = require('cors')
+app.use(cors())
+
+const server = http.createServer(app)
+
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST']
+    }
+})
+
+io.on('connection', (socket) => {
+
+    socket.on('join_room', (data) => {
+        socket.join(data)
+    })
+    
+    socket.on('send_message', (data) => {
+        socket.to(data.room).emit('receive_message', data)
+    })
+})
+
+const PORT = 3001;
+server.listen(PORT, () => console.log(`Server is running on ${PORT}`));
