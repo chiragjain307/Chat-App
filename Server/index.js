c9adfd82c584d294373f9c08162400d29dfada62
chiragjain307 <chiragjain.jain7@gmail.com>
2024-05-06 17:07:04 +0530

    update

--- Server/index.js
+++ Server/index.js
@@ -31,5 +31,5 @@ io.on('connection', (socket) => {
 // const HOST = '192.168.0.106';
 // server.listen(PORT, HOST, () => console.log(`Server is running on http://${HOST}:${PORT}`));
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => console.log(`Server is running on ${PORT}`));
